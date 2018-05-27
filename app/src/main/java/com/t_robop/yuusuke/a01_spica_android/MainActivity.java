f3f6a002da8b4315eb50a0f7d703747f08256048
yuusuke <yuusukeogawara@gmail.com>
2018-05-28 03:08:53 +0900

    importエラーを修正

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/MainActivity.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/MainActivity.java
@@ -4,21 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
-import com.koushikdutta.async.AsyncDatagramSocket;
-import com.koushikdutta.async.AsyncServer;
-
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.util.concurrent.TimeUnit;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
