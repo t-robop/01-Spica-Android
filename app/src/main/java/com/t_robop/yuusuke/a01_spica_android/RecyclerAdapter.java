be771e4e7b5fe194a075954ca56900485d4b547c
yuusuke <yuusukeogawara@gmail.com>
2018-09-19 18:23:30 +0900

    リストアップされた修正点を修正しました。 RecyclerAdapterとlist_item.xmlの内容です。

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/RecyclerAdapter.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/RecyclerAdapter.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
     private OnRecyclerListener recyclerListener;
 
     private ArrayList<ItemDataModel> ItemDataArray;
@@ -31,7 +31,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         TextView textLoopNum;
         ImageView imgLoopBack;
 
-        ViewHolder(View view){
+        ViewHolder(View view) {
             super(view);
             this.linearLayout = view.findViewById(R.id.item_frame);
             this.speedRight = view.findViewById(R.id.text_speed_right);
@@ -39,15 +39,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             this.time = view.findViewById(R.id.text_time);
             this.image = view.findViewById(R.id.direction_item_image);
 
-            this.containerMove=view.findViewById(R.id.container_move);
-            this.containerLoop=view.findViewById(R.id.container_loop);
-            this.containerLoopNum=view.findViewById(R.id.container_loop_text);
-            this.textLoopNum=view.findViewById(R.id.cnt_loop);
-            this.imgLoopBack=view.findViewById(R.id.img_loop_back);
+            this.containerMove = view.findViewById(R.id.container_move);
+            this.containerLoop = view.findViewById(R.id.container_loop);
+            this.containerLoopNum = view.findViewById(R.id.container_loop_text);
+            this.textLoopNum = view.findViewById(R.id.cnt_loop);
+            this.imgLoopBack = view.findViewById(R.id.img_loop_back);
         }
     }
 
-    RecyclerAdapter(ArrayList<ItemDataModel> itemDataList, OnRecyclerListener listener){
+    RecyclerAdapter(ArrayList<ItemDataModel> itemDataList, OnRecyclerListener listener) {
         ItemDataArray = itemDataList;
         recyclerListener = listener;
     }
@@ -55,35 +55,36 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     public ItemDataModel getItem(int position) {
         return ItemDataArray.get(position);
     }
-    public ArrayList<ItemDataModel> getAllItem(){
+
+    public ArrayList<ItemDataModel> getAllItem() {
         return ItemDataArray;
     }
 
     public void setItem(int position, ItemDataModel dataModel) {
-        ItemDataArray.set(position,dataModel);
+        ItemDataArray.set(position, dataModel);
     }
 
     public void addItem(ItemDataModel dataModel) {
         ItemDataArray.add(dataModel);
     }
+
     public void removeItem(int position) {
         ItemDataArray.remove(position);
     }
 
-    public void itemMoved(int fromPos, int toPos){
+    public void moveItem(int fromPos, int toPos) {
 
         ItemDataModel fromItem = ItemDataArray.get(fromPos);
         ItemDataModel toItem = ItemDataArray.get(toPos);
-        ItemDataArray.set(toPos,fromItem);
-        ItemDataArray.set(fromPos,toItem);
+        ItemDataArray.set(toPos, fromItem);
+        ItemDataArray.set(fromPos, toItem);
 
     }
 
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent, false);
         return new ViewHolder(view);
     }
 
@@ -92,71 +93,66 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
         final ViewHolder viewHolder = holder;
 
-        if(ItemDataArray.get(position).getOrderName().equals("loopStart") || ItemDataArray.get(position).getOrderName().equals("loopEnd")){
-            holder.containerLoop.setVisibility(View.VISIBLE);
-            holder.containerMove.setVisibility(View.GONE);
+        if (ItemDataArray.get(position).isLoopBlock()) {
 
-            switch (ItemDataArray.get(position).getOrderName()) {
-                case "loopStart":
-                    holder.containerLoopNum.setOnClickListener(new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view){
-                            recyclerListener.onRecyclerClicked(view, viewHolder.getLayoutPosition());
-                        }
-                    });
-                    holder.containerLoopNum.setVisibility(View.VISIBLE);
-                    holder.textLoopNum.setText(String.valueOf(ItemDataArray.get(position).getLoopCount()));
-                    holder.imgLoopBack.setImageResource(R.drawable.loop_start_block);
-                    break;
-
-                case "loopEnd":
-                    holder.containerLoopNum.setVisibility(View.GONE);
-                    holder.imgLoopBack.setImageResource(R.drawable.loop_end_block);
-                    break;
-            }
-        }else {
-
-            holder.speedRight.setText("右パワー : " + ItemDataArray.get(position).getRightSpeed());
-            holder.speedLeft.setText("左パワー : " + ItemDataArray.get(position).getLeftSpeed());
-            holder.time.setText(ItemDataArray.get(position).getTime() + "秒");
-
-            holder.linearLayout.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
-                    recyclerListener.onRecyclerClicked(view, viewHolder.getLayoutPosition());
-                }
-            });
-
-            holder.containerLoop.setVisibility(View.GONE);
-            holder.containerMove.setVisibility(View.VISIBLE);
-            holder.containerLoopNum.setVisibility(View.GONE);
-
-            switch (ItemDataArray.get(position).getOrderName()) {
-                case "forward":
-                    holder.image.setImageResource(R.drawable.move_front);
-                    break;
-
-                case "back":
-                    holder.image.setImageResource(R.drawable.move_back);
-                    break;
-
-                case "left":
-                    holder.image.setImageResource(R.drawable.move_left);
-                    break;
-
-                case "right":
-                    holder.image.setImageResource(R.drawable.move_right);
-                    break;
-
-                default:
-                    //holder.image.setImageResource(R.drawable.edit_icon);
-                    break;
-            }
+            LoopBlock(viewHolder, position);
+        } else {
+            standardBlock(viewHolder, position);
         }
     }
 
+    private void LoopBlock(final ViewHolder viewHolder, int position) {
+
+        viewHolder.containerLoop.setVisibility(View.VISIBLE);
+        viewHolder.containerMove.setVisibility(View.GONE);
+
+        if (ItemDataArray.get(position).isLoopStartBlock()) {
+            viewHolder.containerLoopNum.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    recyclerListener.onRecyclerClicked(view, viewHolder.getLayoutPosition());
+                }
+            });
+            viewHolder.containerLoopNum.setVisibility(View.VISIBLE);
+            viewHolder.textLoopNum.setText(String.valueOf(ItemDataArray.get(position).getLoopCount()));
+            viewHolder.imgLoopBack.setImageResource(R.drawable.loop_start_block);
+
+        } else if (ItemDataArray.get(position).isLoopEndBlock()) {
+            viewHolder.containerLoopNum.setVisibility(View.GONE);
+            viewHolder.imgLoopBack.setImageResource(R.drawable.loop_end_block);
+        }
+    }
+
+    private void standardBlock(final ViewHolder viewHolder, int position) {
+        viewHolder.speedRight.setText("右パワー : " + ItemDataArray.get(position).getRightSpeed());
+        viewHolder.speedLeft.setText("左パワー : " + ItemDataArray.get(position).getLeftSpeed());
+        viewHolder.time.setText(ItemDataArray.get(position).getTime() + "秒");
+
+        viewHolder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerListener.onRecyclerClicked(view, viewHolder.getLayoutPosition());
+            }
+        });
+
+        viewHolder.containerLoop.setVisibility(View.GONE);
+        viewHolder.containerMove.setVisibility(View.VISIBLE);
+        viewHolder.containerLoopNum.setVisibility(View.GONE);
+
+        if (ItemDataArray.get(position).isStandardForwardBlock()) {
+            viewHolder.image.setImageResource(R.drawable.move_front);
+        } else if (ItemDataArray.get(position).isStandardBackBlock()) {
+            viewHolder.image.setImageResource(R.drawable.move_back);
+        } else if (ItemDataArray.get(position).isStandardLeftBlock()) {
+            viewHolder.image.setImageResource(R.drawable.move_left);
+        } else if (ItemDataArray.get(position).isStandardRightBlock()) {
+            viewHolder.image.setImageResource(R.drawable.move_right);
+        }
+
+    }
+
     @Override
-    public int getItemCount(){
+    public int getItemCount() {
         return ItemDataArray.size();
     }
 
