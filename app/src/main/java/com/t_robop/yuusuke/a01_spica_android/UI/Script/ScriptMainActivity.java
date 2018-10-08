f5b751ec55e5250d72553f17ac849dbdd33d00dc
kawaguchisouya <kawakawa.410487@gmail.com>
2018-10-08 13:48:27 +0900

    RecycleViewの変化をCanvasDataViewに反映できるように。逆はまだ無理。

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/UI/Script/ScriptMainActivity.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/UI/Script/ScriptMainActivity.java
@@ -1,5 +1,6 @@
 package com.t_robop.yuusuke.a01_spica_android.UI.Script;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,12 +9,16 @@ import android.support.v7.widget.RecyclerView;
 import com.t_robop.yuusuke.a01_spica_android.R;
 import com.t_robop.yuusuke.a01_spica_android.model.BlockModel;
 import com.t_robop.yuusuke.a01_spica_android.model.ScriptModel;
+import com.t_robop.yuusuke.a01_spica_android.util.CanvasView;
 
 public class ScriptMainActivity extends AppCompatActivity {
 
     private RecyclerView mScriptRecyclerView;
     private ScriptMainAdapter mScriptAdapter;
     private LinearLayoutManager mScriptLayoutManager;
+    private CanvasView mCanvasView;
+
+    int overallXScroll = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class ScriptMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_script_main);
 
         mScriptRecyclerView = findViewById(R.id.recycler_script);
+        mCanvasView = findViewById(R.id.canvas_view);
 
         mScriptRecyclerView.setHasFixedSize(true);
         mScriptLayoutManager = new LinearLayoutManager(this);
@@ -36,7 +42,53 @@ public class ScriptMainActivity extends AppCompatActivity {
             blockModel.setBlockId(0101+i);
             scriptModel.setBlock(blockModel);
             mScriptAdapter.add(scriptModel);
+
+            mCanvasView.setCommandBlockNum(mScriptAdapter.getItemCount() + 2);
         }
         mScriptAdapter.notifyDataSetChanged();
+
+        final Handler handler = new Handler();
+        final Runnable r = new Runnable() {
+            int count = 0;
+            @Override
+            public void run() {
+
+               // mScriptRecyclerView.scrollToPositionWithOffset();
+                // mScriptLayoutManager.scrollToPositionWithOffset(0,100);
+               // mScriptLayoutManager.getPo
+
+               // mScriptRecyclerView.setScrollX((int)mCanvasView.getPosition());
+//                handler.postDelayed(this, 100);
+
+//                mScriptLayoutManager.scrollVerticallyBy()
+//                mScriptRecyclerView.setScrollY(-1000);
+//                mScriptRecyclerView.setScrollX(-1000);
+                mCanvasView.windowSizeChange(7, (float) mScriptAdapter.getItemCount());
+
+            }
+        };
+        handler.post(r);
+
+
+
+        mScriptRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener(){
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                overallXScroll = overallXScroll + dx;
+                mCanvasView.setPositon(overallXScroll);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+//        int num  = mScriptRecyclerView.computeVerticalScrollRange();
+//        num = mScriptRecyclerView.computeHorizontalScrollRange();
+//        mCanvasView.windowSizeChange(mScriptRecyclerView.getWidth(),mScriptRecyclerView.computeHorizontalScrollRange(), false);
+//        temp = true;
     }
 }
