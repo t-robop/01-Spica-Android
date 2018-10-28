3906515929e4ff7d55825899ff1aca25019a2e4a
yuusuke <yuusukeogawara@gmail.com>
2018-10-29 03:53:53 +0900

    Crashlytics を追加

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/MyApplication.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/MyApplication.java
@@ -1,14 +1,22 @@
 package com.t_robop.yuusuke.a01_spica_android;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MyApplication extends Application {
     private static MyApplication instance = null;
     @Override
     public void onCreate() {
         super.onCreate();
-
         instance = this;
+        Fabric.with(this, new Crashlytics());
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)  // Enables Crashlytics debugger
+                .build();
+        Fabric.with(fabric);
+
     }
 
     public static MyApplication getInstance() {
