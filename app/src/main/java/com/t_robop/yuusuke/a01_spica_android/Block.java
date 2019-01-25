b98b2b9abb2cd12a724ed9fdd3c99e674c338a82
TaigaNatto <taigan3253@gmail.com>
2019-01-25 15:58:22 +0900

    write dream

---
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/Block.java
@@ -0,0 +1,10 @@
+package com.t_robop.yuusuke.a01_spica_android;
+
+public final class Block {
+    // TODO こんな感じの実装がしたい
+    // FIXME ここでResourceファイルを使いたい
+    public final class FrontBlock{
+        public static final String id = "01";
+        public static final String name = "すすむ";
+    }
+}
