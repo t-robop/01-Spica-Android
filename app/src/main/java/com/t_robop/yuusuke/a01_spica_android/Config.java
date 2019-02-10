8b7f19861b16eb8569af26a20ff5a51b1b053a48
Dai1678 <dai7774@gmail.com>
2019-02-10 14:42:07 +0900

    Configクラスにブロックの位置ステータス定義を追加

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/Config.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/Config.java
@@ -1,10 +1,15 @@
 package com.t_robop.yuusuke.a01_spica_android;
 
 public final class Config {
+    //FIXME 実際の速度値(0~255)にする
     public static final int LOW_POWER = 1;
     public static final int MIDDLE_POWER = 2;
     public static final int HIGH_POWER = 3;
 
-    public static final int SENSOR_ABOVE = 1;
-    public static final int SENSOR_BELOW = 2;
+    public static final int SENSOR_ABOVE = 1; //センサー計測値より大きい場合
+    public static final int SENSOR_BELOW = 2; //センサー計測値より小さい場合
+
+    public static final int OUT_OF_IF_LANE = 0; //ブロックが通常レーンにある
+    public static final int IN_TRUE_LANE = 1; //ブロックがifブロック内のtrueレーンにある場合
+    public static final int IN_FALSE_LANE = 2;  //ブロックがifブロック内のfalseレーンにある場合
 }
