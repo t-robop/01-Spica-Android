c7bfcfc371e644e0bb9e69ad3ccb3f7c78572258
Dai1678 <dai7774@gmail.com>
2019-06-06 16:47:43 +0900

    新設定画面への移行完了

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/UI/Script/SettingDetailActivity.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/UI/Script/SettingDetailActivity.java
@@ -1,35 +1,66 @@
 package com.t_robop.yuusuke.a01_spica_android.UI.Script;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.t_robop.yuusuke.a01_spica_android.R;
+import com.t_robop.yuusuke.a01_spica_android.databinding.ActivitySettingDetailBinding;
 
 public class SettingDetailActivity extends AppCompatActivity {
+
+    private ActivitySettingDetailBinding binding;
+    private SharedPreferences preferences;
+
+    private final String preferencePortKey = "port";
+    private final String preferenceSpeedKey = "speed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting_detail);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting_detail);
 
-        Toolbar toolbar = findViewById(R.id.setting_detail_toolbar);
-        setSupportActionBar(toolbar);
+        final String preferenceKey = "udp_config";
+        preferences = getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
+
+        setSupportActionBar(binding.settingDetailToolbar);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
         }
+
+        int port = preferences.getInt(preferencePortKey, 50000);
+        binding.settingDetailPortTextEdit.setText(String.valueOf(port));
+
+        int speed = preferences.getInt(preferenceSpeedKey, 80);
+        binding.settingDetailSpeedTextEdit.setText(String.valueOf(speed));
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            //TODO 保存処理
+            saveSettings();
             finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        saveSettings();
+    }
+
+    private void saveSettings() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(preferencePortKey, Integer.valueOf(binding.settingDetailPortTextEdit.getText().toString()));
+        editor.putInt(preferenceSpeedKey, Integer.valueOf(binding.settingDetailSpeedTextEdit.getText().toString()));
+        editor.apply();
     }
 }
