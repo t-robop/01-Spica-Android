628aaa71e5b23f56324ec7b013ed08d3827b99ba
Dai1678 <dai7774@gmail.com>
2019-06-17 18:57:59 +0900

    fix

--- app/src/main/java/com/t_robop/yuusuke/a01_spica_android/UI/Script/SettingDetailActivity.java
+++ app/src/main/java/com/t_robop/yuusuke/a01_spica_android/UI/Script/SettingDetailActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.t_robop.yuusuke.a01_spica_android.R;
